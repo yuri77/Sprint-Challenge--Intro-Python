338069c2cafa9ea2c473c36827937e3cff191f69
yurika sakae <yurika.sun@gmail.com>
2020-05-19 10:54:36 -0700

    minor fix added

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -40,9 +40,8 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-Starts = ['C', 'D', 'E', 'F', 'G']
-c = [human.name for human in humans if human.name[0]
-     in Starts]
+#Starts = ['C', 'D', 'E', 'F', 'G']
+c = [human.name for human in humans if human.name >= 'C' and human.name < 'H']
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
